7f9f5cef60c97006e8b074d61dd0fc3bc0097766
Chan Jun Wei <chanjunweimy@gmail.com>
2014-05-27 01:13:44 +0800

    wrote autoloading code for oop purpose and user class

---
+++ FindFetchPhp/common/autoload.php
@@ -0,0 +1,4 @@
+<?php
+    spl_autoload_extensions(".php"); // comma-separated list
+    spl_autoload_register();
+?>
---
+++ FindFetchPhp/data/user.php
@@ -0,0 +1,27 @@
+<?php
+namespace FindFetchPhp\data;
+
+class user {
+	private $name;
+	private $password;
+	private $isNus;
+	
+	public function __construct($username, $userpassword, $isNusUser){
+		$this->name = $username;
+		$this->password = $userpassword;
+		$this->isNus = $isNusUser;
+	}
+	
+	public function getName(){
+		return $this->name;
+	}
+	
+	public function isPasswordMatch($userpassword){
+		return $this->password == $userpassword;
+	}
+	
+	public function isNusId(){
+		return $this->isNus;
+	}
+}
+?>
