f538b92f71408b523b9fc39cff65ed754ef3e248
Chan Jun Wei <chanjunweimy@gmail.com>
2014-05-29 19:50:40 +0800

    done implementing UserData: getUser(name) or hasUser(name)
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- FindFetchPhp/data/UserData.php
+++ FindFetchPhp/data/UserData.php
@@ -10,13 +10,65 @@ use exception\DataException;
 class UserData{
 	private $dataManagerInstance = NULL;
 	
+	const USER_DATA_TABLE = "users";
+	const PARAM_NAME = "USERNAME";
+	const PARAM_PASS = "PASSWORD";
+	const PARAM_ISNUS = "ISNUS";
+	
 	public function __construct(){
-		
+		self::initializeUserData();
 	}
 	
 	private function initializeUserData(){
-		
+		$this->dataManagerInstance = new DataManager();
 	}
-	
+
+	/**
+	 * 
+	 * @param String $username
+	 * @throws DataException
+	 * @return \data\User|NULL
+	 */
+	public function getUser($username){
+		$param = self::PARAM_NAME;
+		
+		try {
+			$curUserData = $this->dataManagerInstance->retrieveSingleData($username, $param, self::USER_DATA_TABLE);
+		} catch (DataException $ex){
+			throw $ex;
+		}
+		
+		if ($curUserData){
+			$name = $curUserData[self::PARAM_NAME];
+			$pass = $curUserData[self::PARAM_PASS];
+			$isNus = $curUserData[self::PARAM_ISNUS];
+			
+			$curUser = new User($name, $pass, $isNus);
+			
+			return $curUser;
+		} else {
+			return NULL;
+		}
+	}
+
+	/**
+	 * 
+	 * @param String $username
+	 * @throws DataException
+	 * @return boolean
+	 */
+	public function hasUser($username){
+		try {
+			$curUser = self::getUser($username);
+		} catch (DataException $ex){
+			throw $ex;
+		}
+		
+		if (curUser == NULL){
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
 ?>
