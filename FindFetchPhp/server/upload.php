39a0372e7a7224fef514ca9084a2b982e89da0ea
Chan Jun Wei <chanjunweimy@gmail.com>
2014-06-06 01:22:36 +0800

    user can now upload file to server
    Signed-off-by: Chan Jun Wei <chanjunweimy@gmail.com>

--- FindFetchPhp/server/upload.php
+++ FindFetchPhp/server/upload.php
@@ -1,11 +1,52 @@
 <?php
-	$rootFile = $_SERVER['DOCUMENT_ROOT'];
+	namespace server;
 
-    $file_path = "/uploads/";
+	$rootFile = $_SERVER['DOCUMENT_ROOT'];
+	
+	$dot = ".";
+
+	$filename = $_FILES["uploaded_file"]["name"];
+	$filetype = $_FILES["uploaded_file"]["type"];
+	$filesize = $_FILES["uploaded_file"]["size"];
+	$filetemp = $_FILES["uploaded_file"]["tmp_name"];
+	$fileerror = $_FILES['uploaded_file']['error'];
+	
+    $file_path = "/upload/";
     $file_path = $rootFile . $file_path . basename( $_FILES['uploaded_file']['name']);
-    if(move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $file_path)) {
+    
+    echo $file_path . "<br>";
+    echo "Upload: " . $filename . "<br>";
+    echo "Type: " . $filetype . "<br>";
+    echo "Size: " . ($filesize / 1024) . " kB <br>";
+    echo "Stored in: " . $filetemp . "<br>";
+    
+    $maxFileSize = 1024000; 
+    
+    if ($filesize > $maxFileSize) {
+    	echo "File size too big! File size should be at most {$maxFileSize}/1024 kB";
+    	return;
+    } elseif ($fileerror > 0) {
+    	echo "Error: " . $fileerror . "<br>";
+    	return;
+    } elseif (file_exists($file_path)) {
+    	$rand = 0;
+    	$file_temp_path = $file_path;
+    	
+    	$fileDots = explode($dot , $file_path);
+    	$dotsNum = count($fileDots) - 1;
+    	$fileExtension = $dot . $fileDots[$dotsNum];
+    	
+    	while (file_exists($file_temp_path)){
+    		$file_temp_path = str_replace ($fileExtension , $rand . $fileExtension , $file_path);
+    		$rand++;
+    	}
+    	$file_path = $file_temp_path;
+    } 
+    
+    if (move_uploaded_file($filetemp , $file_path)) {
         echo "success";
-    } else{
+    } else {
         echo "fail";
     }
+    
  ?>
